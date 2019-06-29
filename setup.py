fa48652a69390593abd0b4091c30ea9f6c0318a6
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2019-06-29 18:26:18 +0000

    Allow build with Python 3

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(name="WSGIUtils",
 		'wsgiutils',
 	],
 	package_dir = {'': 'lib'},
+	use_2to3=True,
 	classifiers = [
 		'Development Status :: 5 - Production/Stable',
 		'Intended Audience :: Developers',
